98c412111f95e0bee55271980d6d1a09606caff5
dagamu21 <Moodle1500!>
2025-08-28 23:45:50 -0600

    first commit

---
+++ page-objects/employeesPage.ts
@@ -0,0 +1,10 @@
+import { Page } from "@playwright/test"
+import { HelperBase } from "./helperBase"
+
+export class EmployeesPage extends HelperBase {
+    constructor (page: Page) {
+        super(page)
+    }
+
+    
+}
